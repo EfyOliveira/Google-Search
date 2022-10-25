0bae802a6d9cff7be7f2114792b7f416a9b304b3
EfyOliveira <114318414+EfyOliveira@users.noreply.github.com>
2022-10-24 23:26:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from googlesearch import search
+# pip install google
 
-# pip install Google
-query= "secretotecnicasdeinvasao"
+# Texto a ser pesquisado
+query = "cyberpunk.rs"
 
-def google_search():
-Editando....
+# lang = linguagem da pesquisa "en - ingles ou pt - portugues".
+# num = quantidade de resultado.
+# stop = parar quando tiver 10 resultado.
+# pause = tempo em segudos de intervalo entre resultados.
+def googlesearch()
+  for result in search(query, lang="en", num=10, stop=10, pause=2):
+      print(result)
+      
